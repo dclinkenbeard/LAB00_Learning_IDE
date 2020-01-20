bab96a0206c4ba21622112da3e5dc856c7116a11
Dr.C <dclinkenbeard@csumb.edu>
2020-01-20 10:49:03 -0800

    Initial commit

---
+++ src/Lab00.java
@@ -0,0 +1,62 @@
+public class Lab00 {
+    /**
+     * This is a main method. Any class can have one.
+     * It is what runs when the the class is called from the command line.
+     * @param args
+     */
+    public static void main(String[] args) {
+        //TODO: Add a print statement
+
+//        TODO: Define a
+        mainLoop();
+
+    }
+
+    /**
+     * This is a method. Specifically it is a static method.
+     * What does private mean?
+     * What does static?
+     * What does int mean?
+     *
+     *  This method will start a loop that presents a menu to the user.
+     *  The menu will look like:
+     *      Welcome to the command line calculator!
+     *      Select a choice below:
+     *      0: Quit
+     *      1: add
+     *      2: subtract
+     *      3: multiply
+     *
+     * @return returns a number based on execution status (0 is success)
+     */
+    private static int mainLoop(){
+
+        //TODO: start a loop (a while loop probably makes the most sense)
+        //TODO: Ask for user input (We need scanner)
+        //TODO: use a try catch to prevent errors
+        //TODO: Use a switch statement to call one of the other methods from this class
+
+        return -1; // a negative return value may indicate what?
+    }
+
+    /**
+     * This method takes two numbers and returns the sum.
+     * @param x an integer
+     * @param y an integer
+     * @return returns an integer representing x+y;
+     */
+    private static int adder(int x, int y){
+        return 0;
+    }
+
+    /**
+     * Write a method named subtracter that takes two parameters and returns the difference of x and y
+     *
+     * @param x an integer
+     * @param y an integer
+     * @return returns x - y
+     */
+    // TODO: write the subtracter method here
+
+
+}
